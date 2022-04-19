5c6b56d87c4afeed518811ab996209d066e0ede3
Mitchell Bennett <mitchellbennett@MacBook-Pro-37.attlocal.net>
2022-04-19 10:14:12 -0500

    changed to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
